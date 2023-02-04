54cd0a0b12ae9371a30fbb3539340151bd14d27a
C410 <gcaio7463@gmail.com>
2023-02-03 22:28:15 -0300

    Project with JWT version 1

---
+++ src/main/java/com/api/beatsbrasil/APIBeatsBrasilWebsite/ApiBeatsBrasilWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.api.beatsbrasil.APIBeatsBrasilWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBeatsBrasilWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBeatsBrasilWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/beatsbrasil/APIBeatsBrasilWebsite/ApiBeatsBrasilWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.beatsbrasil.APIBeatsBrasilWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBeatsBrasilWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
